a1073d6ab507449bdbfc16ac3816566668ef7621
fferegrino <antonio.feregrino@gmail.com>
2014-10-21 10:56:13 -0500

    Subiendo las librerías de NuGet porque ???? algo

--- src/Movimientos.Web.Api/App_Start/WebApiConfig.cs
+++ src/Movimientos.Web.Api/App_Start/WebApiConfig.cs
@@ -10,7 +10,7 @@ namespace Movimientos.Web.Api
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            
             // Web API routes
             config.MapHttpAttributeRoutes();
 
