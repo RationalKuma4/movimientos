11803d568d29548a62cf2841b348406d3aaf16be
fferegrino <antonio.feregrino@gmail.com>
2014-10-23 13:41:11 -0500

    xD

--- src/Movimientos.Web.Api.Models/EntityBase.cs
+++ src/Movimientos.Web.Api.Models/EntityBase.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,31 +22,3 @@ namespace Movimientos.Web.Api.Models
 
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Movimientos.Web.Api.Models
-{
-    public abstract class ObjectBase
-    {
-        private List<Link> links;
-
-        public List<Link> Links
-        {
-            get { return links ?? (links = new List<Link>()); }
-            set { links = value; }
-        }
-
-        //lsdkfnsdkvsd
-
-        public void AddLink(Link link)
-        {
-            Links.Add(link);
-        }
-
-    }
-}
->>>>>>> ad7215415c3042a8f91ef3a4763432fbb297ceda
